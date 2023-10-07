041ee4897287f905ed226c1d62637d157c081245
Sebastian Betancourth <betancourth.ingenieria@gmail.com>
2023-10-07 09:27:33 -0500

    feat:se agrega colab

--- routers/home/home.py
+++ routers/home/home.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from config import get_log
 
-from utils.auth import cookie_required, decode_time_token
+from utils.auth import cookie_required
 
 
 log = get_log()
@@ -13,12 +13,12 @@ templates = Jinja2Templates(directory='./public/static/html')
 
 
 @home_router.get(
-    "/index", status_code=200)
-async def home(request: Request, auth: str):
+    "/index", status_code=200, dependencies=[Depends(cookie_required)])
+async def home(request: Request):
     """
 
     """
-    data = cookie_required(auth)
+
     respuesta = templates.TemplateResponse('home.html',
                                            {'request': request})
 
